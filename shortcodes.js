1dc7f283e2e1349c97ca2e2087ac7dd0a3aa8012
Greg Holmes <git@greg.holmes.name>
2023-08-02 10:41:03 +0100

    Add and mandate alt tags for images

--- shortcodes.js
+++ shortcodes.js
@@ -6,11 +6,11 @@ const configFunction = (config, _) => {
     let name = moveAsset(this.page.inputPath, this.page.outputPath, assetName)
     return `<a href="${this.page.url}${name}">${linkText}</a>`
   })
-  config.addAsyncShortcode('image', async function(assetName, width) {
-    return picture(this.page.url, this.page.outputPath, this.page.inputPath, assetName, width)
+  config.addAsyncShortcode('image', async function(assetName, width, alt) {
+    return picture(this.page.url, this.page.outputPath, this.page.inputPath, assetName, width, alt)
   })
-  config.addAsyncShortcode('thumbnail', async function(assetName, width) {
-    return thumbnail(this.page.url, this.page.outputPath, this.page.inputPath, assetName, width)
+  config.addAsyncShortcode('thumbnail', async function(assetName, width, alt) {
+    return thumbnail(this.page.url, this.page.outputPath, this.page.inputPath, assetName, width, alt)
   })
 }
 
--- thumbnailer.js
+++ thumbnailer.js
@@ -1,7 +1,10 @@
 const Image = require('@11ty/eleventy-img')
 const path = require('path')
 
-const picture = async(url, outputFile, inputFile, assetName, width) => {
+const picture = async(url, outputFile, inputFile, assetName, width, alt = "") => {
+  if (alt === "") {
+    return Promise.reject("alt must not be blank")
+  }
   inputFile = path.parse(inputFile)
   inputFile = path.parse(path.join(inputFile.dir, assetName))
   outputFile = path.parse(outputFile)
@@ -18,14 +21,17 @@ const picture = async(url, outputFile, inputFile, assetName, width) => {
     loading: "lazy",
     decoding: "async",
   }
-  return await Image.generateHTML(images, imageAttributes);
+  return Image.generateHTML(images, imageAttributes);
 }
 
-const thumbnail = async (url, outputFile, inputFile, assetName, width) => {
+const thumbnail = async (url, outputFile, inputFile, assetName, width, alt = "") => {
+  if (alt === "") {
+    return Promise.reject("alt must not be blank")
+  }
   inputFile = path.parse(inputFile)
   inputFile = path.parse(path.join(inputFile.dir, assetName))
   outputFile = path.parse(outputFile)
-  const types = ['avif', 'webp', 'jpeg']
+  const types = ['avif', 'webp', 'jpg']
   const images = await Image(path.join(inputFile.dir, inputFile.base), {
     widths: [width],
     formats: types,
@@ -35,14 +41,14 @@ const thumbnail = async (url, outputFile, inputFile, assetName, width) => {
   let output = "<picture>"
   types.forEach(type => {
     if (images[type]) {
-      if (type !== "jpeg") {
+      if (type !== "jpg") {
         images[type].forEach(image => {
-          output += `<source type="image/${image.format}" decoding="async" src="${image.url}" width="${image.width}" height="${image.height}">`
+          output += `<source type="${image.sourceType}" decoding="async" src="${image.url}" width="${image.width}" height="${image.height}">`
         })
       }
     }
   })
-  output += `<img src="${images.jpeg[0].url}" decoding="async" width="${images.jpeg[0].width}" height="${images.jpeg[0].height}">`
+  output += `<img alt=${alt} src="${images.jpeg[0].url}" decoding="async" width="${images.jpeg[0].width}" height="${images.jpeg[0].height}">`
   output += "</picture>"
   return output
 }
